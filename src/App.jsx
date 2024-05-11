116114c509c60d61dcc84070a3932e2f555bb1e8
Anastasiiya Onyshchuk <shapoval.anastasiiya@gmail.com>
2024-05-11 16:51:49 +0300

    add components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
-import Description from "./Description/Description";
-import Options from "./Options/Options";
-import Feedback from "./Feedback/Feedback";
-import Notification from "./Notification/Notification";
+import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
 import clsx from "clsx";
 import "./App.css";
 import { PiSmileyLight } from "react-icons/pi";
